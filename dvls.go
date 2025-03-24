c779602673ceeba4bddf5170ce082efb731a94fb
Danny Bédard <dbedard@devolutions.net>
2025-03-24 14:45:47 -0400

    test(entry_certificate): fix CRUD test order and raw data parsing

--- dvls.go
+++ dvls.go
@@ -20,6 +20,8 @@ type RequestError struct {
 	Err error
 }
 
+const defaultContentType string = "application/json"
+
 type RequestOptions struct {
 	ContentType string
 	RawBody     bool
@@ -36,26 +38,28 @@ func (c *Client) Request(url string, reqMethod string, reqBody io.Reader, option
 		return Response{}, &RequestError{Err: fmt.Errorf("failed to fetch login status. error: %w", err), Url: url}
 	}
 	if !islogged {
-		err := c.refreshToken()
+		err := c.login()
 		if err != nil {
 			return Response{}, &RequestError{Err: fmt.Errorf("failed to refresh login token. error: %w", err), Url: url}
 		}
 	}
 
-	resp, err := c.rawRequest(url, reqMethod, reqBody, options...)
+	var opts RequestOptions
+	if len(options) > 0 {
+		opts = options[0]
+	}
+
+	resp, err := c.rawRequest(url, reqMethod, defaultContentType, reqBody, opts)
 	if err != nil {
 		return Response{}, err
 	}
 	return resp, nil
 }
 
-func (c *Client) rawRequest(url string, reqMethod string, reqBody io.Reader, options ...RequestOptions) (Response, error) {
-	contentType := "application/json"
-	var rawBody bool
-
+func (c *Client) rawRequest(url string, reqMethod string, contentType string, reqBody io.Reader, options ...RequestOptions) (Response, error) {
+	var opts RequestOptions
 	if len(options) > 0 {
-		contentType = options[0].ContentType
-		rawBody = options[0].RawBody
+		opts = options[0]
 	}
 
 	req, err := http.NewRequest(reqMethod, url, reqBody)
@@ -69,7 +73,11 @@ func (c *Client) rawRequest(url string, reqMethod string, reqBody io.Reader, opt
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return Response{}, &RequestError{Err: fmt.Errorf("error while submitting request. error: %w", err), Url: url}
-	} else if resp.StatusCode != http.StatusOK {
+	}
+	defer resp.Body.Close()
+
+	// Check for unexpected status codes
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
 		return Response{}, &RequestError{Err: fmt.Errorf("unexpected status code %d", resp.StatusCode), Url: url}
 	}
 
@@ -78,13 +86,22 @@ func (c *Client) rawRequest(url string, reqMethod string, reqBody io.Reader, opt
 	if err != nil {
 		return Response{}, &RequestError{Err: fmt.Errorf("failed to read response body. error: %w", err), Url: url}
 	}
-	defer resp.Body.Close()
 
-	if !rawBody {
-		err = json.Unmarshal(response.Response, &response)
-		if err != nil {
-			return response, &RequestError{Err: fmt.Errorf("failed to unmarshal response body. error: %w", err), Url: url}
-		}
+	// If RawBody is true, return the raw response without further processing
+	if opts.RawBody {
+		return response, nil
+	}
+
+	// Handle empty response bodies for successful requests
+	if len(response.Response) == 0 {
+		response.Message = "Empty response (success)"
+		return response, nil
+	}
+
+	// Otherwise, unmarshal the response as JSON
+	err = json.Unmarshal(response.Response, &response)
+	if err != nil {
+		return response, &RequestError{Err: fmt.Errorf("failed to unmarshal response body. error: %w", err), Url: url}
 	}
 
 	return response, nil
